966f9e0a3b839351e516a07b3a8b0f8b53291635
MaanasVohra <maanasvohra1998@gmail.com>
2018-03-20 00:26:24 +0530

    Added functionality for maps

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+var mysql = require("mysql");
+
+// connection to database;
+
+var con  = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "xxxx",
+    database: "rajasthan_hackathon"
+});
+
+con.connect(function(err){
+    if(err) {
+        console.log("Error connecting to database");
+    } else {
+        console.log("Connected to database!");
+    }
+});
+
+app.get("/", function(req, res){
+    var sqlStatement = "select * from markers;";
+    con.query(sqlStatement, function(err, result, fields){
+        if(err) {
+            console.log("Error querying!");
+        } else {
+            res.render("maps.ejs", {result: result});
+        }
+    });
+    // res.sendFile(__dirname + "/maps1.html");
+});
+
+app.listen(3000, function(){
+    console.log("Server initiated!");
+});
